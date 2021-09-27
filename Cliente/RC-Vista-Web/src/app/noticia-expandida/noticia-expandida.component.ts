ef4a6436dcf17fd016f0588ae426c8356a721056
ClaudiaJH <90428660+ClaudiaJH@users.noreply.github.com>
2021-09-26 22:24:32 -0400

    Parte Unida

--- Cliente/RC-Vista-Web/src/app/noticia-expandida/noticia-expandida.component.ts
+++ Cliente/RC-Vista-Web/src/app/noticia-expandida/noticia-expandida.component.ts
@@ -25,14 +25,6 @@ export class NoticiaExpandidaComponent implements OnInit {
   contenido:any;
   fecha:any;
   categoria:any;
-  /*public noticiaEntrante:NoticiaPrincipal={
-    idNoticia: 0,
-    titulo: 'error 404',
-    imagen: '',
-    contenido: '',
-    fecha: '',
-    categoria: ''
-  };*/
   
   ngOnInit(): void {
     this.idNoticia = sessionStorage.getItem('idNoticia');
@@ -46,14 +38,6 @@ export class NoticiaExpandidaComponent implements OnInit {
     this.comentariosCortados = this.comentarios.slice(0,this.numeroDivisionComentarios);
     this.cantidadComentarios = this.comentarios.length;
   }
-  /*public noticiaprincipal:NoticiaPrincipal= {
-    idNoticia: this.noticiaEntrante.idNoticia,
-    titulo: this.noticiaEntrante.titulo,
-    imagen: this.noticiaEntrante.imagen,
-    contenido: this.noticiaEntrante.contenido,
-    fecha: this.noticiaEntrante.fecha,
-    categoria: this.noticiaEntrante.categoria
-  };*/
   cantidadItems = 8;
   paginaActual = 1;
 
@@ -178,33 +162,21 @@ export class NoticiaExpandidaComponent implements OnInit {
   },
   ]
 
-  
-
-  relacionados:Relacionados[] = [
-    {
-      idNoticia:1,
-      titulo:"ONU asevera que los talibanes ya han “refugiado” a miembros de Al Qaeda",
-      imagen:"assets/images/afganistan.jpg ",
-      contenido:"La Paz, 10 de septiembre (RC).- La enviada especial de la ONU para Afganistán, Deborah Lyons, informó que los talibanes ya han “acogido y refugiado visiblemente” a miembros de Al Qaeda, por lo que pidió a las autoridades y organizaciones internacionales que se unan para evitar el colapso de la economía de Afganistán, según el portal Infobae. Del mismo modo, la representante de la ONU explicó que el Consejo de Seguridad de la ONU deberá determinar cómo ser relacionará con los 33 miembros del gobierno talibán que están en la lista negra de sanciones de la ONU, ya que varias de las autoridades de dicho sector son personas buscadas por la ley. Por otro lado, Lyons advirtió de “una crisis inminente” causada por la congelación de miles de millones de dólares en activos afganos. La enviada de la ONU explicó que dicha situación podría causar una recesión económica que dejaría a millones de afganos en la pobreza.",
-      fecha:"11/09/2021",
-      autor:"Radio Compañera",
-      categoria:"Social"
-    },
-    {idNoticia:2,
-      titulo:"Caso Anakin: Padre de la víctima asegura que abogada de Álvaro intenta cambiar los sucesos del crimen",
-    imagen:"assets/images/noticia2.jpg",
-    contenido:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbb",
-    fecha:"11/09/2021",
-    autor:"Radio Compañera",
-    categoria:"Social"
+  noticiasCostado:NoticiasCostado[] = [
+    {idNoticia:1,titulo:"La actual Miss Universo publicó una foto en la que muestra sus pies y le ‘llovieron’ los comentarios",
+    imagen:"assets/images/noticia16.jpg",
+    contenido:"La Miss Universo, la mexicana Andrea Meza, ha levantado críticas, pero también ha recibido apoyo. Esta vez una foto que se tomó en Nueva York y que publicó en sus redes sociales ha convertido a sus pies en el centro de la atención. En la imagen se la ve luciendo un conjunto casual que deja ver sus piernas y también las sandalias que calza. De inmediato le llovieron las críticas en las redes y es que por la foto parecería que la reina universal tiene los dedos de los pies torcidos y que el calzado no le queda bien. Ni ella ni la Organización Miss Universo (OMU) se han pronunciado, ni han retirado de circulación la fotografía.Los seguidores más radicales de los concursos de belleza critican que sus dedos no sean como como el estándar y que esa foto no la favorece, que la OMU debería supervisar todo el material que sus reinas suben a las redes sociales, que se ve muy mal que una Miss Universo dé esa imagen de 'desprolijidad'. Estos críticos le aconsejan utilizar zapatos cerrados, informa el diario La Opinión.",
+    fecha:"11/09/2021",categoria:"Social"
   },
-    {idNoticia:3,
-      titulo:"Fiscalía solicita detención preventiva para los acusados de la muerte de Mayerly",
-    imagen:"assets/images/noticia3.jpg"
-    ,contenido:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbb",
-    fecha:"11/09/2021",
-    autor:"Radio Compañera",
-    categoria:"Social"
+    {idNoticia:2,titulo:"Sergio Ramírez denuncia en Madrid el exilio forzado que le impone Ortega",
+    imagen:"assets/images/noticia18.jpg",
+    contenido:"Antes, el gobierno español rechazó las infundadas acusaciones contra el literato, ganador en 2017 del máximo galardón de las letras españolas, el Premio Cervantes. Ramírez ha demostrado siempre su compromiso en la defensa de la democracia en su país, afirmaron en un comunicado conjunto el ministerio de Asuntos Exteriores y el de Cultura, recordando su papel en la lucha contra el régimen de los Somoza desde el movimiento sandinista, al lado precisamente del actual presidente, Daniel Ortega.El ex presidente colombiano Juan Manuel Santos, que se encuentra en Madrid con la antigua candidata presidencial Ingrid Betancourt, presentando el libro de ambos Una conversación pendiente, tildó de oprobioso lo que está haciendo Ortega.  Es oprobioso, es la demostración de algo que está sucediendo en América Latina en muchos países, y está in crescendo, que es esa tendencia al autoritarismo y prácticamente hacia la dictadura, afirmó Santos.Tongolele no sabe bailar, la nueva obra del nicaragüense, es una entrega de las peripecias del inspector Dolores Morales, ambientada en las protestas de 2018, duramente reprimidas por el gobierno de Ortega.Las relaciones entre España y Nicaragua se tensaron a raíz de la campaña de detenciones de opositores, entre ellos siete aspirantes a la presidencia en las elecciones generales del 7 de noviembre.El presidente Daniel Ortega, de 75 años y en el poder desde 2007, se postula para un cuarto mandato sucesivo.",
+    fecha:"11/09/2021",categoria:"Social"
+  },
+    {idNoticia:3,titulo:"China hace millonaria donación a Afganistán sin obviar la amenaza terrorista",
+    imagen:"assets/images/noticia19.jpg",
+    contenido:"En una reunión con los ministros de Exteriores de los países vecinos de Afganistán, el canciller Wang Yi anunció la donación de 31 millones de dólares en ayudas a Afganistán para apoyar su recuperación. El diplomático pidió gestos similares a Estados Unidos y sus aliados para que asuman sus responsabilidades y advirtió del riesgo terrorista que aún se cierne sobre el país.Una vez anunciado el nuevo Gobierno de los talibanes en Afganistán, China ha empezado a mover sus fichas.El pasado miércoles 8 septiembre tuvo lugar una reunión virtual entre los ministros de Exteriores de los países limítrofes con Afganistán: China, Pakistán, Irán, Tayikistán, Uzbekistán y Turkmenistán.En la cumbre, el canciller chino, Wang Yi, anunció el envío de 31 millones de dólares en ayudas a Afganistán. Se trata de 200 millones de yuanes en grano, suministros para el invierno, vacunas y medicamentos, que se suman a otro lote de tres millones de dosis de vacunas contra el Covid-19. El ministro destacó que los países vecinos, más que nadie, quieren ayudar a que Afganistán salga del caos.Por ello, el diplomático aseguró que China está dispuesta a ayudar al país a construir nuevos proyectos de infraestructuras que hagan más fácil la vida de los ciudadanos afganos y, en la medida de sus posibilidades, apoyar la reconstrucción pacífica y el desarrollo económico una vez haya seguridad en el país.",
+    fecha:"11/09/2021",categoria:"Social"
   },
   ]
   //--------------reproductor----------
@@ -227,23 +199,16 @@ export class NoticiaExpandidaComponent implements OnInit {
     ];
   //----------------comentarios-------------------------
   comentarios:Comentarios[]=[
-    {idComentario:1,idNoticia:1, textoComentario:"Soy un comentario1", fecha:"2021-9-10",nombreUsuario:"bonbon"},
-    {idComentario:2,idNoticia:1, textoComentario:"Soy un comentario2", fecha:"2021-1-10",nombreUsuario:"burbuja"},
-    {idComentario:3,idNoticia:1, textoComentario:"Soy un comentario3", fecha:"2021-9-1",nombreUsuario:"bellota"},
-    {idComentario:4,idNoticia:1, textoComentario:"Soy un comentario4", fecha:"2021-9-25",nombreUsuario:"nose"},
+    {idComentario:1,idNoticia:1, textoComentario:"Muy buena la informacion en este apartado", fecha:"2021-9-10",nombreUsuario:"Sofia Rodriguez"},
+    {idComentario:2,idNoticia:1, textoComentario:"Interesante, me sirvio mucho.", fecha:"2021-7-10",nombreUsuario:"Diego Fernadez"},
+    {idComentario:3,idNoticia:1, textoComentario:"Impresionante lo de los politicos", fecha:"2021-9-1",nombreUsuario:"Lorena Herrera"},
+    {idComentario:4,idNoticia:1, textoComentario:"Me parecio bastante interesante toda la informacion recopilada.", fecha:"2021-9-25",nombreUsuario:"Dross Rotzank"},
   ]
-  /*
-  comentarios:Comentarios[]=[
-    {idNoticia:1,idComentario:1, textoComentario:"Soy un comentario1", idUsuario:1,nombreUsuario:"bonbon", bLike:false, bDislike:false},
-    {idNoticia:1,idComentario:2, textoComentario:"Soy un comentario2", idUsuario:2,nombreUsuario:"burbuja",bLike:false, bDislike:false},
-    {idNoticia:1,idComentario:3, textoComentario:"Soy un comentario3", idUsuario:3,nombreUsuario:"bellota",bLike:false, bDislike:false},
-  ]
-  */
+
   numeroDivisionComentarios = 5;
   comentariosCortados:Comentarios[] | any;
   cantidadComentarios = 0;
-  //booleanLike: boolean = false;
-  //booleanDislike: boolean = false;
+
   aumentarComentariosVista(){
     this.numeroDivisionComentarios = this.numeroDivisionComentarios + 5;
     this.comentariosCortados=this.comentarios.slice(0,this.numeroDivisionComentarios);
@@ -261,30 +226,7 @@ export class NoticiaExpandidaComponent implements OnInit {
     return diferencia;
   }
 
-  /*cambiarColorLike(posicion:number,booleanLike:boolean, booleanDislike:boolean) {
-    if(booleanLike == false && booleanDislike == false){
-      booleanLike = true;      
-    } else if(booleanLike == true){
-      booleanLike = false;
-    } else if(booleanDislike == true){
-      booleanLike = true;
-      booleanDislike = false;
-    }
-    this.comentarios[posicion].bLike = booleanLike;
-    this.comentarios[posicion].bDislike = booleanDislike;
-  }
-  cambiarColorDislike(posicion:number,booleanLike:boolean, booleanDislike:boolean) {
-    if(booleanLike == false && booleanDislike == false){
-      booleanDislike = true;
-    } else if(booleanDislike == true){
-      booleanDislike = false;
-    } else if(booleanLike == true){
-      booleanDislike = true;
-      booleanLike = false;
-    }
-    this.comentarios[posicion].bLike = booleanLike;
-    this.comentarios[posicion].bDislike = booleanDislike;
-  }*/
+
   enviarComentario(textoComentario:string, textoUsuario:string){
     this.comentarios.reverse();
     this.comentarios.push({idComentario:5,idNoticia:1, textoComentario:textoComentario, fecha:this.nowDate(),nombreUsuario:textoUsuario});
@@ -300,16 +242,7 @@ interface Comentarios{
   textoComentario: string;
   nombreUsuario: string;
 }
-/*
-interface Comentarios{
-  idNoticia: number;
-  idComentario: number;
-  textoComentario: string;
-  idUsuario: number;
-  nombreUsuario: string;
-  bLike: boolean;
-  bDislike: boolean;
-}*/
+
 interface Noticias {
   idNoticia: number;
   titulo: string;
@@ -320,22 +253,12 @@ interface Noticias {
   categoria: string;
 }
 
-/*interface NoticiaPrincipal {
-  idNoticia: number;
-  titulo: string;
-  imagen: string;
-  contenido: string;
-  fecha: string;
-  //autor: string;
-  categoria: string;
-}*/
 
-interface Relacionados {
+interface NoticiasCostado {
   idNoticia: number;
   titulo: string;
   imagen: string;
   contenido: string;
   fecha: string;
-  autor: string;
   categoria: string;
 }
